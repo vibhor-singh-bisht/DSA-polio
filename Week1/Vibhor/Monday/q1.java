55224af9c77637577dd75194ca6ee93be63a5b2a
vibhor-singh-bisht <singhvibhor95@gmail.com>
2026-08-31 22:38:38 +0530

    Solution 1 brute force

---
+++ Week1/Vibhor/Monday/q1.java
@@ -0,0 +1,15 @@
+package Week1.Vibhor.Monday;
+
+public class q1 {
+        public int[] twoSum(int[] nums, int target) {
+
+        int [] result = new int[2];
+        for(int i = 0 ; i < nums.length -1 ; i++){
+            for(int j = i+1; j < nums.length ; j++)
+                if(nums[i] + nums[j] == target){
+                    result = new int[] {i,j};
+                }
+        }
+        return result;
+    }
+}
