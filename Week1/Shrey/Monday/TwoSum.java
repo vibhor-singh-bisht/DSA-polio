cb5c5f757c8e6c35ed9192ab5f033b4ad0b23119
Shrey Johri <johrishrey1@gmail.com>
2026-08-31 23:39:03 +0530

    hashmap solution for Twosum

--- Week1/Shrey/Monday/TwoSum.java
+++ Week1/Shrey/Monday/TwoSum.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] brute(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -15,16 +16,30 @@ public class TwoSum {
     // above approach is the brute force
     // time complexity - O(N^2)
 
+    public int[] hashmapSolution(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if (map.containsKey(value)) {
+                return new int[] { map.get(value), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("no such numbers add up to target value");
+    }
 
-    
+    // above approach is optimised with Hashmap and only one loop
+    // time complexity is O(n)
 
     public static void main(String[] args) {
         TwoSum obj = new TwoSum();
         int[] nums = { 2, 4, 7, 5, 11 };
         int target = 9;
 
-        int[] result = obj.twoSum(nums, target);
+        int[] result = obj.brute(nums, target);
+        int[] resultwithhashmap = obj.hashmapSolution(nums, target);
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(resultwithhashmap));
 
     }
 }
