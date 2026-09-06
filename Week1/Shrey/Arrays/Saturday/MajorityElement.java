fd7b4a344a65142a5e8ae7039471da333a8f9bcb
Shrey Johri <69461111+shreyjohri@users.noreply.github.com>
2026-09-06 15:28:53 +0530

    moores

--- Week1/Shrey/Arrays/Saturday/MajorityElement.java
+++ Week1/Shrey/Arrays/Saturday/MajorityElement.java
@@ -35,7 +35,25 @@ public class MajorityElement {
     }
 
     public int majEleMoore(int[] nums) {
-         
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return count > nums.length / 2 ? candidate : -1;
     }
 
     public static void main(String[] args) {
