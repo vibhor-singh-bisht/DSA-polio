76b74bd398207e146ffbc6d5540e6f1a9fd56737
vibhor-singh-bisht <singhvibhor95@gmail.com>
2026-09-07 18:19:21 +0530

    MondayQ1Q2

---
+++ Week2/Vibhor/Q2ContainsDuplicate.java
@@ -0,0 +1,33 @@
+package Week2.Vibhor;
+
+public class Q2ContainsDuplicate {
+    class Solution {
+    public int maxSubArray(int[] nums) {
+    int sum = 0; 
+    int maxSum = Integer.MIN_VALUE;
+    for( int i = 0 ; i < nums.length ; i++){
+        sum+=nums[i];
+        if(sum > maxSum){
+            maxSum = sum;
+        }
+        if(sum == 0){
+            maxSum = 0;
+        }
+    }
+    return maxSum;
+    }
+}
+
+    public static void main(String[] args) {
+
+        Q2ContainsDuplicate solution = new Q2ContainsDuplicate();
+        Solution obj = solution.new Solution();
+
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int result = obj.maxSubArray(nums);
+
+        System.out.println(result);
+    }
+    
+}
