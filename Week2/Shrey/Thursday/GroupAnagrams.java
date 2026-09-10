3b48b0038dc77e7c236d08dd1592a8cb6c0b6f2b
Shrey Johri <johrishrey1@gmail.com>
2026-09-10 18:01:16 +0530

    Anal Gram bccc

---
+++ Week2/Shrey/Thursday/GroupAnagrams.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            // Count frequency of each letter in the string
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int num : count) {
+                sb.append(num).append("#");
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams obj1 = new GroupAnagrams();
+
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        List<List<String>> res = obj1.groupAnagrams1(strs);
+        System.out.println(res);
+
+    }
+
+}
