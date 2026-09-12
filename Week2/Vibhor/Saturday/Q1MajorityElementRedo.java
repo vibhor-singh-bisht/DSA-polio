082017f85e43136d91364dc153854ac4c001db47
vibhor-singh-bisht <singhvibhor95@gmail.com>
2026-09-12 15:32:36 +0530

    FlabbergastedAfterDoingIn1try

---
+++ Week2/Vibhor/Saturday/Q1MajorityElementRedo.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int x = nums[0];
+
+            for( int i = 0 ; i < nums.length ; i++){
+                if(nums[i] == x){
+                    count++;
+                }
+                else if( nums[i] != x && count > 0 ) count --;
+
+                else x = nums[i];
+            }
+            return x;
+    }
+}
