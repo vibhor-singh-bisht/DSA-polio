2e2707889dbbd05f39a5a7370b9ecde944331c06
rohitbasra <rohitbasra76@gmail.com>
2026-09-14 20:17:50 +0530

    Day 13 - Rohit

---
+++ Week3/Rohit/Q1.java
@@ -0,0 +1,10 @@
+package Week3.Rohit;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+}
