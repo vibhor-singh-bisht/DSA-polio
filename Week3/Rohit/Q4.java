994df640731157301892ba7e58b4f41ed34d1d4c
rohitbasra <rohitbasra76@gmail.com>
2026-09-15 23:05:58 +0530

    Day 14 - Rohit

---
+++ Week3/Rohit/Q4.java
@@ -0,0 +1,38 @@
+package Week3.Rohit;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+
+        int [] prefixSum = new int[nums.length];
+
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+
+        prefixSum[0] = nums[0];
+        map.put(prefixSum[0],0);
+
+        for(int i=1;i<nums.length;i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+            map.put(prefixSum[i], i);
+        }
+
+        int [] ans = new int[queries.length];
+        int i =0;
+        for(int query: queries){
+            Map.Entry<Integer,Integer> entry = map.floorEntry(query);
+
+            if(entry != null){
+                ans[i] = entry.getValue() + 1;
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
+}
+
